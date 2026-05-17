f8551c1dd7e3532607d036ed23efb8f307030489
mr-ops-bobo <mrilyacraft@gmail.com>
2026-05-17 15:40:20 +0300

    Add files via upload

---
+++ common/txt.bat
@@ -0,0 +1,11 @@
+echo off
+chcp 65001
+rem Создать новый каталог
+md firstcat  
+rem Перейти в новый каталог
+cd firstcat  
+md secondcat 
+cd secondcat 
+md thirdcat 
+rem this need for pause of programm 
+pause
